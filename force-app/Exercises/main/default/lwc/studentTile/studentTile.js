6ee5639ebf651372d3b6c2be336775ba855b756f
Carlos Paesano <cpaesano@xtriam.com>
2023-03-10 11:37:52 -0800

    Exercise 2-7A Completed

--- force-app/Exercises/main/default/lwc/studentTile/studentTile.js
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -6,13 +6,15 @@ export default class StudentTile extends LightningElement {
         PhotoUrl: '/services/images/photo/003B0FakePictId',
     };
     @api isSelected = false;
-    
+
     get tileSelected() {
         return this.isSelected ? "tile selected" : "tile";
     }
-    studentClick(){
-        alert(this.student.Name);
-        }
+    studentClick() {
+        // alert(this.student.Name);
+        debugger;
+        console.log('Name', this.student.Name);
+        console.log('Photo', this.student.PhotoUrl);
+    }
 }
 
-  
